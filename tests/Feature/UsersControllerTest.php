0075dfd2060efee1cbc9d0dc7b8dc3f1ea0d5afa
Qusai Farraj <qusai919@gmail.com>
2023-03-28 18:19:34 -0500

    add tests

---
+++ tests/Feature/UsersControllerTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Tests\Feature;
+
+ use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\User;
+use Tests\TestCase;
+
+class UsersControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic test example.
+     */
+    public function test_users_can_be_retrieved(): void
+    {
+        User::factory()->count(10)->create();
+
+        $response = $this->get(route('api.users'));
+
+        $response->assertStatus(200);
+        $this->assertDatabaseCount(User::class, 10);
+        $this->assertEquals(10, $response->json('data'));
+    }
+
+    public function test_user_can_be_created(): void
+    {
+        $data = [
+            'name' => 'tester phpunit',
+            'email' => fake()->email,
+            'employee_id' => generateEmployeeId(),
+        ];
+        $response = $this->get(route('api.users.create', $data));
+
+        $response->assertStatus(201);
+        $this->assertDatabaseHas(User::class, $data);
+    }
+
+    public function test_user_can_be_edited(): void
+    {
+        $user = User::factory()->create();
+
+        $data = [
+            'name' => 'tester phpunit',
+            'email' => fake()->email,
+            'employee_id' => generateEmployeeId(),
+        ];
+        $response = $this->get(route('api.users.edit', array_merge(['user' => $user->id], $data)));
+
+        $response->assertStatus(201);
+        $this->assertDatabaseHas(User::class, $data);
+    }
+
+    public function test_user_can_be_retrieved(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this->get(route('api.users.edit', ['user' => $user->id]));
+
+        $response->assertStatus(200);
+        $response->assertSeeText($user->name);
+        $response->assertSeeText($user->email);
+    }
+}
+
